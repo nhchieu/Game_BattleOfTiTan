06e66ded2de7a126713e4c3aef0a798753505220
nhchieu <nhchieu271@gmail.com>
2024-12-13 14:51:24 +0700

    update boss

--- Assets/Codes/Spawner.cs
+++ Assets/Codes/Spawner.cs
@@ -27,14 +27,20 @@ public class Spawner : MonoBehaviour
     }
     void spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(0);
-        enemy.transform.position = spawPoint[Random.Range(1, spawPoint.Length)].position;
-        enemy.GetComponent<Enemy>().Init(spawnData[level]);
-        if (level >= 2)
+        if (level < 3)
+        {
+            GameObject enemy = GameManager.instance.pool.Get(0);
+            enemy.transform.position = spawPoint[Random.Range(1, spawPoint.Length)].position;
+            enemy.GetComponent<Enemy>().Init(spawnData[level]);
+        }
+        else
         {
             GameObject enemy1 = GameManager.instance.pool.Get(0);
             enemy1.transform.position = spawPoint[Random.Range(1, spawPoint.Length)].position;
-            enemy1.GetComponent<Enemy>().Init(spawnData[level-2]);
+            Vector3 newScale = enemy1.transform.localScale * 4f; // tang scale
+            enemy1.transform.localScale = newScale;
+            enemy1.GetComponent<Enemy>().Init(spawnData[level]);
+            spawnData[level].spawnTime = 10000;
         }
 
     }
